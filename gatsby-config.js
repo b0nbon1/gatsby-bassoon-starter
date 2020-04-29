9265d38539fe9986ab6041575d9232d728aec250
Bonvic Bundi <nyabuyabonvic@gmail.com>
2020-04-29 05:58:15 +0200

    reset default configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Bassoon Starter`,
+    description: `Kick off your next, great Gatsby project with this bassoon starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@bonvic7`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,16 +15,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    'gatsby-plugin-theme-ui',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-starter-bassoon`,
+        short_name: `bassoon`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: ``, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
